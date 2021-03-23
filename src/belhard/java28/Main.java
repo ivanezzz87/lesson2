041aaabf8b808b60183cdfa70961bec84c27f4e0
”Ivan <mseptk@yandex.ru>
2021-03-23 23:15:13 +0300

    third commit

--- src/belhard/java28/Main.java
+++ src/belhard/java28/Main.java
@@ -3,6 +3,20 @@ package belhard.java28;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!!!");
-    }
+        int yearOfBirthday = 1987;
+        int monthOfBirthday = 1;
+        int dayOfBirthday = 9;
+        int sum = yearOfBirthday + monthOfBirthday + dayOfBirthday;
+        boolean monthBiggerDay = false;
+        if (monthOfBirthday > dayOfBirthday) {
+            monthBiggerDay = true;
+        }
+        System.out.println("Сумма моего года, месяца и дня рождения:" + sum);
+        System.out.println("Месяц моего рождения больше даты рождения:" + monthBiggerDay);
+        char [] name = new char[]{'И', 'В', 'А', 'Н'};
+        System.out.println("Массив с моим именем:" + String.valueOf(name));
+        double myAge = 34.0;
+        double partYear = 2.0 / 12.0;
+        myAge += partYear;
+        System.out.println("Мне " + myAge + " лет");
 }
