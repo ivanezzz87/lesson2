e37743ed2abdfc365e85af7d4381884bb2862b72
”Ivan <mseptk@yandex.ru>
2021-03-29 07:22:30 +0300

    Исправление ошибок

--- src/belhard/java28/Main.java
+++ src/belhard/java28/Main.java
@@ -7,16 +7,14 @@ public class Main {
         int monthOfBirthday = 1;
         int dayOfBirthday = 9;
         int sum = yearOfBirthday + monthOfBirthday + dayOfBirthday;
-        boolean monthBiggerDay = false;
-        if (monthOfBirthday > dayOfBirthday) {
-            monthBiggerDay = true;
-        }
+        boolean monthBiggerDay = monthOfBirthday > dayOfBirthday;
         System.out.println("Сумма моего года, месяца и дня рождения:" + sum);
         System.out.println("Месяц моего рождения больше даты рождения:" + monthBiggerDay);
-        char [] name = new char[]{'И', 'В', 'А', 'Н'};
+        char[] name = new char[]{'И', 'В', 'А', 'Н'};
         System.out.println("Массив с моим именем:" + String.valueOf(name));
         double myAge = 34.0;
         double partYear = 2.0 / 12.0;
         myAge += partYear;
         System.out.println("Мне " + myAge + " лет");
+    }
 }
